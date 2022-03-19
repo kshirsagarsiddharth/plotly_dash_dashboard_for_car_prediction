9282ea91640e1a0af309d81322f23cfa47fb5b2d
Siddharth Kshirsagar <kshirsagarsiddharth45@gmail.com>
2022-03-20 00:15:20 +0530

    done

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import plotly.graph_objects as go
 import joblib
-from utils import row1, row2, row3, row4, row5
+from utils import row1, row2, row3, row4, row5, navbar
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output, State
 import dash_bootstrap_components as dbc
@@ -10,42 +10,42 @@ import dash_daq as daq
 df = pd.read_csv('car_price_data.csv').drop('Unnamed: 0', axis=1)
 
 loaded_model = joblib.load('final_car_prediction_model.pkl')
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__, external_stylesheets=[dbc.themes.SUPERHERO])
 
 app.layout = html.Div([
+    navbar,
     html.Div([row1,
+              # html.Hr(),
               row2,
+              #   html.Hr(),
               row3,
+              #   html.Hr(),
               row4,
               dbc.Button("Submit", className="me-1",
                          id='submit-button-state', color="dark",),
-              ], className='container'),
+              ], className='container',style={'padding-top': '25px'}),
 
     #html.Button(id='submit-button-state', n_clicks=0, children='Submit'),
 
 
     html.Div([
-         row5,
-        html.Div(id='year-picker-output'),
-        html.Div(id='engine-size-slider-output'),
-
-        html.Div(id='km-per-liters-slider-output'),
-        html.Div(id='model-dropdown-output'),
-        html.Div(id='transmission-dropdown-output'),
-        html.Div(id='fuel-type-dropdown-output'),
-        html.Div(id='prediction', style={'font-size': '60px'})
+        html.Div([
+            row5,
+            html.Div(id='model-dropdown-output', style={'font-size': '30px'}),
+            html.Div(id='prediction', style={'font-size': '60px'})],
+            className="container"
+        ),
     ])
 
 ])
 
 
 @app.callback(
-    Output('year-picker-output', 'children'),
-    Output('engine-size-slider-output', 'children'),
-    Output('km-per-liters-slider-output', 'children'),
-    Output('model-dropdown-output', 'children'),
-    Output('transmission-dropdown-output', 'children'),
-    Output('fuel-type-dropdown-output', 'children'),
+
+
+
+
+
     Output('prediction', 'children'),
 
     Input('submit-button-state', 'n_clicks'),
@@ -64,33 +64,83 @@ def update_output(n_clicks, km_travelled, date_value, engine_size, tax_value, km
                km_per_liters, model_name, transmission, fuel_type]],
         columns=joblib.load('column_names.pkl')))
 
-   
+    return (
 
-    return (f"date_value: {date_value}",
-            f"engine_size: {engine_size}",
-            f"Km Per Liters: {km_per_liters}",
-            f"model name: {model_name}",
-            f"transmission: {transmission}",
-            f"fuel_type: {fuel_type}",
-            f"prediction: {prediction}"
-            )
+
+        f"Car Price: {round(prediction[0])}"
+    )
+
 
 @app.callback(Output('km-travelled-gauge', 'value'), Input('km-travelled-slider', 'value'))
 def km_travelled_update(km_travelled):
     return km_travelled
 
-@app.callback( Output('tax-slider-output', 'figure'), Input('tax-slider', 'value'),)
-def tax_update(tax_value):
-   return go.Figure(go.Indicator(
-    mode = "number+delta",
-    value = tax_value,
-    delta = {"reference": df['tax'].mean()},
-    title = {"text": "Tax Entered(relative to average)"},
-    domain = {'x': [0, 1], 'y': [0, 1]}
-    ))
-   
+
+@app.callback(Output('km-per-liters-slider-output', 'value'), Input('km-per-liters-slider', 'value'))
+def km_travelled_update(km_per_lit):
+    return km_per_lit
 
 
+@app.callback(Output('engine-size-slider-output', 'value'), Input('engine-size-slider', 'value'))
+def km_travelled_update(engine_slider):
+    return engine_slider
+
+
+def return_card_content(header, value_to_update, info=None):
+    card_content = [
+        dbc.CardHeader(header),
+        dbc.CardBody(
+            [
+                html.H5(str(value_to_update), className="card-title"),
+                html.P(
+                    info,
+                    className="card-text",
+                ),
+            ]
+        ),
+    ]
+    return card_content
+
+
+@app.callback(Output('tax-slider-output', 'children'), Input('tax-slider', 'value'),)
+def year_update(tax_value):
+    card_content = [
+        dbc.CardHeader("Tax Slider"),
+        dbc.CardBody(
+            [
+                html.H5(str(tax_value), className="card-title"),
+                html.P(
+                    "tax charged",
+                    className="card-text",
+                ),
+            ]
+        ),
+    ]
+    return card_content
+
+
+@app.callback(Output('year-picker-output', 'children'), Input('year-picker', 'value'),)
+def tax_update(year):
+    card_content = return_card_content("Year", year, "Model Year")
+    return card_content
+
+
+@app.callback(Output('fuel-type-dropdown-output', 'children'), Input('fuel-type-dropdown', 'value'),)
+def fuel_update(fuel_type):
+    card_content = return_card_content("Fuel Type", fuel_type, "Type Of Fuel")
+    return card_content
+
+
+@app.callback(Output('transmission-dropdown-output', 'children'), Input('transmission-dropdown', 'value'),)
+def transmisssion_update(transmission_type):
+    card_content = return_card_content(
+        "Transmission Type", transmission_type, "Type Of Fuel")
+    return card_content
+
+
+@app.callback(Output('model-dropdown-output', 'children'), Input('model-dropdown', 'value'),)
+def transmisssion_update(model_name):
+    return f"Selected Model: {model_name}",
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -9,80 +9,117 @@ df = pd.read_csv('car_price_data.csv').drop('Unnamed: 0', axis=1)
 
 
 row1 = dbc.Row([
-    dbc.Col(html.H5('KM Travelled')),
-    dbc.Col(dcc.Slider(
+
+    dbc.Col([html.H6("KM Travelled"),
+             dcc.Slider(
         min=df['km_traveled'].min(),
         max=df['km_traveled'].max(),
         value=df['km_traveled'].min(),
         id='km-travelled-slider',
         tooltip={"placement": "bottom"}
-    )),
-    dbc.Col(html.H5('Pick Car Year')),
-    dbc.Col(dcc.Dropdown(
+    )], className="col-sm"),
+
+
+    dbc.Col([html.H6('Year'),
+             dcc.Dropdown(
         id='year-picker',
         options=np.sort(df['year'].unique()),
-        value=2017
-    ))
-])
+        value=2017,
+         className='dropdown-class'
+    )])
+], class_name="row sm-2")
 
 row2 = dbc.Row([
-    dbc.Col(html.H5('tax-picker'),),
-    dbc.Col(dcc.Slider(
-        min=df['tax'].min(),
-        max=df['tax'].max(),
-        value=df['tax'].min(),
-        id='tax-slider',
-        tooltip={"placement": "bottom"}
-    ),),
-    dbc.Col(html.H5('engine-size'),),
-    dbc.Col(dcc.Slider(
-        min=df['engineSize'].min(),
-        max=df['engineSize'].max(),
-        value=df['engineSize'].min(),
-        id='engine-size-slider',
-        marks={i: str(i) for i in np.sort(df['engineSize'].unique())},
-        tooltip={"placement": "bottom"}
-    ),)
-])
+
+    dbc.Col([
+        html.H6('Tax'),
+        dcc.Slider(
+            min=df['tax'].min(),
+            max=df['tax'].max(),
+            value=df['tax'].min(),
+            id='tax-slider',
+            tooltip={"placement": "bottom"}
+        ),
+    ]),
+
+    dbc.Col([
+        html.H6('Engine Size'),
+        dcc.Slider(
+            min=df['engineSize'].min(),
+            max=df['engineSize'].max(),
+            value=df['engineSize'].min(),
+            id='engine-size-slider',
+            tooltip={"placement": "bottom"}
+        ),
+    ])
+], className="mb-2",)
 
 
 row3 = dbc.Row([
-    dbc.Col(html.H5('km-per-liter'),),
-             dbc.Col(dcc.Slider(
-                 min=df['km_per_liters'].min(),
-                 max=df['km_per_liters'].max(),
-                 value=df['km_per_liters'].min(),
-                 id='km-per-liters-slider',
-                 tooltip={"placement": "bottom"}
-             ),), 
 
-   dbc.Col(html.H5('model-dropdown'),),
-    dbc.Col(dcc.Dropdown(
-        id='model-dropdown',
-        options=np.sort(df['model'].unique()),
-        value=df['model'][0]
-    ),)
+    dbc.Col([
+        html.H6('KM/L'),
+        dcc.Slider(
+            min=df['km_per_liters'].min(),
+            max=df['km_per_liters'].max(),
+            value=df['km_per_liters'].min(),
+            id='km-per-liters-slider',
+            tooltip={"placement": "bottom"}
+        ),
+    ]),
+
+
+    dbc.Col([
+        html.H6('Model'),
+        dcc.Dropdown(
+            id='model-dropdown',
+            options=np.sort(df['model'].unique()),
+            value=df['model'][0],
+            className='dropdown-class'
+        ),
+    ])
 ])
 
 row4 = dbc.Row([
-    dbc.Col(html.H5('transmission-dropdown'),),
-    dbc.Col(dcc.Dropdown(
-        id='transmission-dropdown',
-        options=np.sort(df['transmission'].unique()),
-        value=df['transmission'][0]
-    ),),
-    dbc.Col(html.H5('fuel-type-dropdown'),),
-    dbc.Col(dcc.Dropdown(
-        id='fuel-type-dropdown',
-        options=np.sort(df['fuel_type'].unique()),
-        value=df['fuel_type'][0]
-    ),)
+    dbc.Col([
+        html.H6('Transmission'),
+        dcc.Dropdown(
+            id='transmission-dropdown',
+            options=np.sort(df['transmission'].unique()),
+            value=df['transmission'][0],
+            className='dropdown-class'
+        ),
+    ]),
+
+    dbc.Col([
+        html.H6('Fuel Type'),
+        dcc.Dropdown(
+            id='fuel-type-dropdown',
+            options=np.sort(df['fuel_type'].unique()),
+            value=df['fuel_type'][0],
+            className='dropdown-class',
+            
+        ),
+    ])
 ])
 
 
+card_content = [
+    # dbc.CardHeader("Card header"),
+    # dbc.CardBody(
+    #     [
+    #         html.H5("Card title", className="card-title"),
+    #         html.P(
+    #             "This is some card content that we'll reuse",
+    #             className="card-text",
+    #         ),
+    #     ]
+    # ),
+]
+
 row5 = dbc.Row([
     dbc.Col(
-         daq.Gauge(
+        daq.Gauge(
             id='km-travelled-gauge',
             label="KM Travelled Entered",
             value=df['km_traveled'].min(),
@@ -90,9 +127,79 @@ row5 = dbc.Row([
             max=df['km_traveled'].max(),
             showCurrentValue=True,
             units="KM",
-    ),
-    ),
+
+        ),
+        class_name="col-sm-2"),
+
     dbc.Col(
-        dcc.Graph(id='tax-slider-output')
-    )
-])
+        daq.Gauge(
+            id='km-per-liters-slider-output',
+            label="KM/L Entered",
+            value=df['km_per_liters'].min(),
+            min=df['km_per_liters'].min(),
+            max=df['km_per_liters'].max(),
+            showCurrentValue=True,
+            units="KM/L",
+
+        ),
+        class_name="col-sm-2"),
+
+    dbc.Col(
+        daq.Tank(
+            id='engine-size-slider-output',
+            label="Engine Size",
+            value=df['engineSize'].min(),
+            min=df['engineSize'].min(),
+            max=df['engineSize'].max(),
+            showCurrentValue=True,
+            units="CC",
+
+        ),
+        class_name="col-sm-2"),
+
+    dbc.Col([
+        dbc.Row(
+            [
+                dbc.Col(dbc.Card(card_content,
+                        id='tax-slider-output', className='card-class')),
+                dbc.Col(dbc.Card(card_content,
+                        id='year-picker-output')),
+
+            ],
+
+        ),
+
+        dbc.Row(
+            [
+                dbc.Col(dbc.Card(card_content,
+                        id='fuel-type-dropdown-output')),
+                dbc.Col(dbc.Card(card_content,
+                        id='transmission-dropdown-output')),
+
+            ],
+
+        ),
+    ])
+], style={'border-style': 'solid'})
+
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Page 1", href="#")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+            
+        ),
+    ],
+    brand="Car Price Prediction dashboard",
+    brand_href="#",
+   
+    
+)
